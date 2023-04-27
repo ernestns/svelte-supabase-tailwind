50a355c14a6f01f64f3b15d923948acd0e9ba54a
Ernest Soeralaya <esoeralaya@gmail.com>
2023-04-27 02:48:44 -0500

    try tailwind

--- src/lib/Auth.svelte
+++ src/lib/Auth.svelte
@@ -23,6 +23,9 @@
 <div class="row flex-center flex">
   <div class="col-6 form-widget" aria-live="polite">
     <h1 class="header">Supabase + Svelte</h1>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     <p class="description">Sign in via magic link with your email below</p>
     <form class="form-widget" on:submit|preventDefault="{handleLogin}">
       <div>
